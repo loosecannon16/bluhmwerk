e3217868a39fd8ed1d39908f77701fe179557f78
Pedro Bluhm <Pedro Bluhm>
2017-01-24 17:45:32 -0200

    inicial

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // Load Gulp
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    clean = require('gulp-clean'),
     plugins = require('gulp-load-plugins')({
         rename: {
             'gulp-live-server': 'serve'
@@ -8,7 +9,7 @@ var gulp = require('gulp'),
     });
 
 // Start Watching: Run "gulp"
-gulp.task('default', ['watch']);
+gulp.task('default', ['build-css', 'build-js', 'dist-index', 'server', 'squish-jquery']);
 
 // Run "gulp server"
 gulp.task('server', ['serve', 'watch']);
@@ -71,6 +72,7 @@ gulp.task('watch', function () {
     gulp.watch('assets/js/libs/**/*.js', ['squish-jquery']);
     gulp.watch('assets/js/*.js', ['build-js']);
     gulp.watch('assets/less/**/*.less', ['build-css']);
+    gulp.watch('index.html', ['dist-index']);
 });
 
 // Folder "/" serving at http://localhost:8888
@@ -81,4 +83,14 @@ gulp.task('serve', function () {
     gulp.watch(['build/*'], function (file) {
         server.notify.apply(server, [file]);
     });
+});
+
+gulp.task('dist-index', function(){
+    return gulp.src('index.html')
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('clean', function(){
+    return gulp.src('build/')
+        .pipe(clean());
 });
